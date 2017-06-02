c79ddd928c997bcbddaf095a63acf59e74105a7d
Dragos Foianu <dragos.foianu@gmail.com>
2017-06-03 00:43:39 +0300

    AlgorithmsI - MergeSort

---
+++ AlgorithmsI/AlgorithmsI/Courses/Sort/MergeSort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsI.Courses.Sort
+{
+    /**
+    * Operations:
+    *  Merge:     O(n)
+    *  Sort:      O(logn)
+    *  Total:     O(nlogn)
+    */
+    public class MergeSort
+    {
+        private static void Merge(ref IComparable[] a, ref IComparable[] aux, int lo, int mid, int hi)
+        {
+            int i = lo;
+            int j = mid + 1;
+
+            for (int k = lo; k <= hi; k++) {
+                aux[k] = a[k];
+            }
+
+            for (int k = lo; k <= hi; k++)
+            {
+                if (i > mid)
+                    a[k] = aux[j++];
+                else if (j > hi)
+                    a[k] = aux[i++];
+                else if (aux[j].CompareTo(aux[i]) < 0)
+                    a[k] = aux[j++];
+                else
+                    a[k] = aux[i++];
+            }
+        }
+
+        private static void Sort(ref IComparable[] a, ref IComparable[] aux, int lo, int hi)
+        {
+            if (hi <= lo) return;
+            int mid = lo + (hi - lo) / 2;
+            Sort(ref a, ref aux, lo, mid);
+            Sort(ref a, ref aux, mid + 1, hi);
+            Merge(ref a, ref aux, lo, mid, hi);
+        }
+
+        public static void Sort(ref IComparable[] a)
+        {
+            IComparable[] aux = new IComparable[a.Length];
+            Sort(ref a, ref aux, 0, a.Length - 1);
+        }
+    }
+}
---
+++ AlgorithmsI/UnitTestAlgorithmsI/Courses/Sort/TestMergeSort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmsI.Courses.UnionFind;
+using System.Collections.Generic;
+
+using AlgorithmsI.Courses.Sort;
+
+namespace UnitTestAlgorithmsI
+{
+    [TestClass]
+    public class TestMergeSort
+    {
+        private Random rnd;
+
+        [TestInitialize]
+        public void MergeSort_Init()
+        {
+            rnd = new Random();
+        }
+
+        [TestMethod]
+        public void MergeSort_EmptyList()
+        {
+            IComparable[] a = new IComparable[0];
+            MergeSort.Sort(ref a);
+        }
+
+        [TestMethod]
+        public void MergeSort_RandomInts()
+        {
+            /* Randomize numbers 0..49 */
+            IComparable[] a = Enumerable.Range(0, 50).OrderBy(r => rnd.Next()).Select(x => x as IComparable).ToArray();
+            MergeSort.Sort(ref a);
+            
+            /* Make sure they are sorted */
+            for (int i = 1; i < a.Length; i++)
+            {
+                Assert.AreEqual(true, a[i - 1].CompareTo(a[i]) < 0);
+            }
+        }
+    }
+}
