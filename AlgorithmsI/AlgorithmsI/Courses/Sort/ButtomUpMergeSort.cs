732dccd03e0c2da85c16eccdd6b57cfd77c35a5a
Dragos Foianu <dragos.foianu@gmail.com>
2017-06-03 23:10:15 +0300

    AlgorithmsI - Bottom-up MergeSort

---
+++ AlgorithmsI/AlgorithmsI/Courses/Sort/ButtomUpMergeSort.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsI.Courses.Sort
+{
+    /**
+    * Operations:
+    *  Merge:     O(n)
+    *  Sort:      O(logn)
+    *  Total:     O(nlogn)
+    */
+    public class BottomUpMergeSort
+    {
+        private static void Merge(ref IComparable[] a, ref IComparable[] aux, int lo, int mid, int hi)
+        {
+            int i = lo;
+            int j = mid + 1;
+
+            for (int k = lo; k <= hi; k++) {
+                aux[k] = a[k];
+            }
+
+            for (int k = lo; k <= hi; k++)
+            {
+                if (i > mid)
+                    a[k] = aux[j++];
+                else if (j > hi)
+                    a[k] = aux[i++];
+                else if (aux[j].CompareTo(aux[i]) < 0)
+                    a[k] = aux[j++];
+                else
+                    a[k] = aux[i++];
+            }
+        }
+
+        public static void Sort(ref IComparable[] a)
+        {
+            int N = a.Length;
+            IComparable[] aux = new IComparable[N];
+            
+            for (int sz = 1; sz < N; sz = sz + sz)
+            {
+                for (int lo = 0; lo < N - sz; lo += sz + sz)
+                {
+                    Merge(ref a, ref aux, lo, lo + sz - 1, Math.Min(lo + sz + sz - 1, N - 1));
+                }
+            }
+        }
+    }
+}
--- AlgorithmsI/UnitTestAlgorithmsI/Courses/Sort/TestMergeSort.cs
+++ AlgorithmsI/UnitTestAlgorithmsI/Courses/Sort/TestMergeSort.cs
@@ -27,12 +27,33 @@ namespace UnitTestAlgorithmsI
         }
 
         [TestMethod]
+        public void BottomUpMergeSort_EmptyList()
+        {
+            IComparable[] a = new IComparable[0];
+            MergeSort.Sort(ref a);
+        }
+
+        [TestMethod]
         public void MergeSort_RandomInts()
         {
             /* Randomize numbers 0..49 */
             IComparable[] a = Enumerable.Range(0, 50).OrderBy(r => rnd.Next()).Select(x => x as IComparable).ToArray();
             MergeSort.Sort(ref a);
             
+            /* Make sure they are sorted */
+            for (int i = 1; i < a.Length; i++)
+            {
+                Assert.AreEqual(true, a[i - 1].CompareTo(a[i]) < 0);
+            }
+        }
+
+        [TestMethod]
+        public void BottomUpMergeSort_RandomInts()
+        {
+            /* Randomize numbers 0..49 */
+            IComparable[] a = Enumerable.Range(0, 50).OrderBy(r => rnd.Next()).Select(x => x as IComparable).ToArray();
+            BottomUpMergeSort.Sort(ref a);
+
             /* Make sure they are sorted */
             for (int i = 1; i < a.Length; i++)
             {
