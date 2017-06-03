7759b6b7f6a2b32eda1682c7ef5aa423d257ffb4
Dragos Foianu <dragos.foianu@gmail.com>
2017-06-04 02:04:33 +0300

    AlgorithmsI - Knuth uniform array shuffle

---
+++ AlgorithmsI/AlgorithmsI/Courses/Other/KnuthShuffle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsI.Courses
+{
+    public class KnuthShuffle
+    {
+        private static Random rnd;
+
+        static KnuthShuffle()
+        {
+            rnd = new Random();
+        }
+
+        public static void Shuffle(ref IComparable[] a)
+        {
+            int N = a.Length;
+
+            for (int i = 0; i < N; i++)
+            {
+                /* Random between 0 and i (inclusive) */
+                int r = rnd.Next(i + 1);
+
+                /* Swap */
+                IComparable tmp = a[i];
+                a[i] = a[r];
+                a[r] = tmp;
+            }
+        }
+    }
+}
