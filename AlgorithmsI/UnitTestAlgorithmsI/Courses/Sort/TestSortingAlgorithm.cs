dbbae6f8e8a702b35f0f13b43519a1bff66566f2
Dragos Foianu <dragos.foianu@gmail.com>
2017-06-04 00:10:13 +0300

    Refactor SortingAlgorithm tests to remove duplicated code

--- AlgorithmsI/UnitTestAlgorithmsI/Courses/Sort/TestSortingAlgorithm.cs
+++ AlgorithmsI/UnitTestAlgorithmsI/Courses/Sort/TestSortingAlgorithm.cs
@@ -9,37 +9,57 @@ using AlgorithmsI.Courses.Sort;
 namespace UnitTestAlgorithmsI
 {
     [TestClass]
-    public class TestMergeSort
+    public class TestSortingAlgorithm
     {
         private Random rnd;
-        private SortingAlgorithm sort;
 
         [TestInitialize]
         public void MergeSort_Init()
         {
             rnd = new Random();
-            sort = new MergeSort();
         }
 
-        [TestMethod]
-        public void MergeSort_EmptyList()
+        private void TestEmptyList(ISortingAlgorithm sort)
         {
             IComparable[] a = new IComparable[0];
             sort.Sort(ref a);
         }
 
-        [TestMethod]
-        public void MergeSort_RandomInts()
+        private void TestRandomInts(ISortingAlgorithm sort)
         {
             /* Randomize numbers 0..49 */
             IComparable[] a = Enumerable.Range(0, 50).OrderBy(r => rnd.Next()).Select(x => x as IComparable).ToArray();
             sort.Sort(ref a);
-            
+
             /* Make sure they are sorted */
             for (int i = 1; i < a.Length; i++)
             {
                 Assert.AreEqual(true, a[i - 1].CompareTo(a[i]) < 0);
             }
+        }
+
+        [TestMethod]
+        public void MergeSort_EmptyList()
+        {
+            TestEmptyList(new MergeSort());
+        }
+
+        [TestMethod]
+        public void MergeSort_RandomInts()
+        {
+            TestRandomInts(new MergeSort());
+        }
+
+        [TestMethod]
+        public void BottomUpMergeSort_EmptyList()
+        {
+            TestEmptyList(new BottomUpMergeSort());
+        }
+
+        [TestMethod]
+        public void BottomUpMergeSort_RandomInts()
+        {
+            TestRandomInts(new BottomUpMergeSort());
         }
     }
 }
