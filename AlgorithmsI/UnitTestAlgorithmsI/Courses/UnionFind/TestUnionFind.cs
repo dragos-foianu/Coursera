7993e8d355159d8b5ce0c9c366708e3a592e348c
Dragos Foianu <dragos.foianu@gmail.com>
2017-06-04 00:06:17 +0300

    Refactor UnionFind tests to remove duplicated code

--- AlgorithmsI/UnitTestAlgorithmsI/Courses/UnionFind/TestUnionFind.cs
+++ AlgorithmsI/UnitTestAlgorithmsI/Courses/UnionFind/TestUnionFind.cs
@@ -1,13 +1,13 @@
 ﻿using System;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using AlgorithmsI.Courses.UnionFind;
 using System.Collections.Generic;
 
-namespace UnitTestAlgorithmsI
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmsI.Courses.UnionFind;
+
+namespace UnitTestAlgorithmsI.Courses.UnionFind
 {
     [TestClass]
-    public class TestQuickFind
+    public class TestUnionFind
     {
         private int CountConnectedComponents(List<int> components)
         {
@@ -15,21 +15,21 @@ namespace UnitTestAlgorithmsI
             return set.Count;
         }
 
-        [TestMethod]
-        public void QuickFind_Union()
+        private void TestUnion(IUnionFind uf)
         {
-            QuickFind qf = new QuickFind(100);
+            Assert.AreEqual(100, uf.Components().Count, "The given UnionFind algorithm must be defined for 100 nodes");
+
             List<int> components;
 
             /* Test union of two nodes */
-            qf.Union(0, 1);
-            components = qf.Components;
+            uf.Union(0, 1);
+            components = uf.Components();
             Assert.AreEqual(components[0], components[1]);
             Assert.AreEqual(CountConnectedComponents(components), components.Count - 1);
 
             /* Test union of three nodes */
-            qf.Union(1, 2);
-            components = qf.Components;
+            uf.Union(1, 2);
+            components = uf.Components();
             Assert.AreEqual(components[0], components[1]);
             Assert.AreEqual(components[1], components[2]);
             Assert.AreEqual(CountConnectedComponents(components), components.Count - 2);
@@ -42,57 +42,93 @@ namespace UnitTestAlgorithmsI
                 int p = random.Next(0, components.Count);
                 int q = random.Next(0, components.Count);
                 connections.Add(new Tuple<int, int>(p, q));
-                qf.Union(p, q);
+                uf.Union(p, q);
             }
 
-            components = qf.Components;
+            components = uf.Components();
             foreach (var con in connections)
             {
                 Assert.AreEqual(components[con.Item1], components[con.Item2]);
             }
         }
 
-        [TestMethod]
-        public void QuickFind_Find()
+        private void TestFind(IUnionFind uf)
         {
-            QuickFind qf = new QuickFind(100);
+            Assert.AreEqual(100, uf.Components().Count, "The given UnionFind algorithm must be defined for 100 nodes");
+
             bool result;
 
             /* Initially, I find no path */
-            result = qf.Find(0, 1);
+            result = uf.Find(0, 1);
             Assert.AreEqual(result, false);
 
             /* Test directly connected nodes */
-            qf.Union(0, 1);
-            result = qf.Find(0, 1);
+            uf.Union(0, 1);
+            result = uf.Find(0, 1);
             Assert.AreEqual(result, true);
 
             /* Test indirectly connected nodes */
-            qf.Union(1, 2);
-            result = qf.Find(0, 2);
+            uf.Union(1, 2);
+            result = uf.Find(0, 2);
             Assert.AreEqual(result, true);
 
             /* Test non-connected nodes */
-            result = qf.Find(0, 9);
+            result = uf.Find(0, 9);
             Assert.AreEqual(result, false);
 
             /* Test find in an union of all nodes */
             Random random = new Random();
-            List<int> components = qf.Components;
+            List<int> components = uf.Components();
             for (int i = 3; i < components.Count; i++)
             {
-                qf.Union(i - 1, i);
+                uf.Union(i - 1, i);
             }
 
-            components = qf.Components;
-            for (int i = 0; i < 100; i++)
+            components = uf.Components();
+            for (int i = 0; i < components.Count; i++)
             {
                 int p = random.Next(0, components.Count);
                 int q = random.Next(0, components.Count);
-                result = qf.Find(p, q);
+                result = uf.Find(p, q);
                 Assert.AreEqual(result, true);
             }
             Assert.AreEqual(CountConnectedComponents(components), 1);
+        }
+
+        [TestMethod]
+        public void QuickFind_Union()
+        {
+            TestUnion(new QuickFind(100));
+        }
+
+        [TestMethod]
+        public void QuickUnion_Union()
+        {
+            TestUnion(new QuickFind(100));
+        }
+
+        [TestMethod]
+        public void QuickUnionWeighted_Union()
+        {
+            TestUnion(new QuickFind(100));
+        }
+
+        [TestMethod]
+        public void QuickFind_Find()
+        {
+            TestUnion(new QuickFind(100));
+        }
+
+        [TestMethod]
+        public void QuickUnion_Find()
+        {
+            TestUnion(new QuickFind(100));
+        }
+
+        [TestMethod]
+        public void QuickUnionWeighted_Find()
+        {
+            TestUnion(new QuickFind(100));
         }
     }
 }
